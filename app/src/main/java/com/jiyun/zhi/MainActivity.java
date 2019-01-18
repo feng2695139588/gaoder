30ba701cd61ea26f38d6b3ac02231c9a901acfea
luofeng <2695139588@qq.com>
2019-01-18 15:45:58 +0800

    上传Utils工具类

--- app/src/main/java/com/jiyun/zhi/MainActivity.java
+++ app/src/main/java/com/jiyun/zhi/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String s = new String();
-
         s="asdfgfh";
 
     }
+
 }
